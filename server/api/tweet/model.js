bfa30200cd23b7bdf70a08cf10ee07765de61998
Pehuén <pehuen.rodriguez@nayra.coop>
2020-03-19 16:01:27 -0300

    #5 Tweet model initial structure

--- server/api/tweet/model.js
+++ server/api/tweet/model.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 
 const TweetSchema = mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
+  tweet_created_at: { type: String, trim: true, required: true, index: true },
+  tweet_id_str: { type: String, trim: true, required: true, index: true },
+  full_text: { type: String, trim: true },
+  hastags: {
+    type: [{ type: String, trim: true, }],
     required: true,
-    index: true,
-  }
+  },
+  media: {
+    type: [{
+      media_url: { type: String, trim: true }
+      media_url_https: { type: String, trim: true }
+    }],
+    required: true,
+  },
+  user: {
+    name: { type: String, trim: true, required: true },
+    screen_name: { type: String, trim: true, required: true },
+    location: { type: String, trim: true },
+    profile_image_url: { type: String, trim: true },
+    profile_image_url_https: { type: String, trim: true },
+  },
+  geo: { },
+  coordinates: { },
 }, { collection: 'tweet' });
 
 module.exports = {
