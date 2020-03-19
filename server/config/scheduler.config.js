ccb76c6152ea15b4bfd85e133f0111c5dda309a0
Pehuén <pehuen.rodriguez@nayra.coop>
2020-03-19 16:48:58 -0300

    #2 Some order

--- server/config/scheduler.config.js
+++ server/config/scheduler.config.js
@@ -1,10 +1,15 @@
 const schedule = require('node-schedule');
+const { getTweets } = require('../CRON/twitter');
  
 class SchedulerConfig {
   static init() {
-    return schedule.scheduleJob('10 * * * *', () => {
-      console.log('The answer to life, the universe, and everything!');
-    });
+    if (process.env.CRON_ACTIVE && process.env.CRON_ACTIVE === "true") {
+      return schedule.scheduleJob('10 * * * *', () => {
+        getTweets(null);
+      });
+    } else {
+      console.log(".env variable CRON_ACTIVE was not set to 'true' or undefined. CRON to fetch Tweets will not run.")
+    }
   }
 }
 
