6a0bbc025e3282c2412239d4722f07d4997f9bc4
Pehuén <pehuen.rodriguez@nayra.coop>
2020-03-22 20:32:03 -0300

    user.id_str and media sizes

--- server/api/tweet/model.js
+++ server/api/tweet/model.js
@@ -16,10 +16,12 @@ const TweetSchema = mongoose.Schema({
       media_url_small: { type: String, trim: true },
       media_url_medium: { type: String, trim: true },
       media_url_large: { type: String, trim: true },
+      sizes: [{ }],
     }],
     required: true,
   },
   user: {
+    id_str: { type: String, trim: true, required: true, index: true },
     name: { type: String, trim: true, required: true },
     screen_name: { type: String, trim: true, required: true },
     location: { type: String, trim: true },
