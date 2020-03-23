5721606d3aca1707af493087085a2d30d926720a
Hernán Gigena <hernangigena@fiqus.com>
2020-03-23 15:12:04 -0300

    Agregar count de usuarios de twitter a la cache.

--- server/api/twitter_users/controller.js
+++ server/api/twitter_users/controller.js
@@ -2,18 +2,28 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 const assert = require("assert");
 
+const { CacheConfig } = require("../../config/cache.config");
 const { TwitterUsersDAO } = require("./dao");
 
 class TwitterUsersController {
 
   async getUsersCount(req, res, next) {
     try {
+      const cache = CacheConfig.get();
+      const key = `twitter_users_count`;
+      const value = cache.get(key);
+
+      if (value) {
+        return res.status(200).json(value);
+      }
+
       const usersCount = await TwitterUsersDAO.findOne();
       if (!usersCount) {
         return res.status(404).send({
           message: "twitter users count not found"
         });
       }
+      cache.set(key, usersCount);
       res.status(200).json(usersCount);
     } catch (error) {
       console.error(error);
