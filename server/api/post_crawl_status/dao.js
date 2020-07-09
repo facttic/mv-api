b63bde8b93fa08fd969c570fc64e8c9ded9029e7
Pehuén <pehuen.rodriguez@nayra.coop>
2020-07-09 19:37:22 -0300

    Move to post crawl status

---
+++ server/api/post_crawl_status/dao.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-use-before-define */
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+const moment = require("moment");
+
+const { PostCrawlStatusSchema } = require("./model");
+
+PostCrawlStatusSchema.statics.createNew = async function createNew(postCrawlStatus) {
+  const _postCrawlStatus = new PostCrawlStatusDAO(postCrawlStatus);
+  const newPostCrawlStatus = await _postCrawlStatus.save();
+  return newPostCrawlStatus;
+}
+
+PostCrawlStatusSchema.statics.getLast = async function getLast(source) {
+  // post_id_str: -1 biggest on top
+  const lastPostCrawlStatus = await PostCrawlStatusDAO.findOne({ source }).sort({post_id_str: -1}).exec();
+  return lastPostCrawlStatus;
+};
+
+PostCrawlStatusSchema.plugin(mongooseDelete, { deletedAt: true, deletedBy: true, overrideMethods: true, indexFields: ["deleted"] });
+
+const PostCrawlStatusDAO = mongoose.model("PostCrawlStatus", PostCrawlStatusSchema);
+
+module.exports = { PostCrawlStatusDAO };
---
+++ server/api/post_crawl_status/model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PostCrawlStatusSchema = mongoose.Schema({
+  post_id_str: { type: String, trim: true, required: true, index: true },
+  post_created_at: { type: String, trim: true, required: true },
+  source: { type: String, trim: true, required: true },
+}, { collection: 'post_crawl_status' });
+
+module.exports = {
+  PostCrawlStatusSchema,
+};
