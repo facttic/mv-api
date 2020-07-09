44813a3dac4a12be96e7c42c6d842f0bbd25ff2e
Pehuén <pehuen.rodriguez@nayra.coop>
2020-07-09 16:23:02 -0300

    Remove unused socket

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const { DBConfig } = require('./config/db.conf');
 const { RoutesConfig } = require('./config/routes.config');
-const { SocketIoConfig } = require('./config/socket.io.config');
+// const { SocketIoConfig } = require('./config/socket.io.config');
 const { SchedulerConfig } = require('./config/scheduler.config');
 const { CacheConfig } = require('./config/cache.config');
 
@@ -17,7 +17,7 @@ RoutesConfig.init(app, express.Router());
 SchedulerConfig.init();
 CacheConfig.init();
 
-const httpServer = http.createServer(app).listen(PORT, HOST, () => {
-  console.log(`Server listening at ${HOST}:${PORT}`);
-  SocketIoConfig.init(httpServer);
-});
+// const httpServer = http.createServer(app).listen(PORT, HOST, () => {
+//   console.log(`Server listening at ${HOST}:${PORT}`);
+//   SocketIoConfig.init(httpServer);
+// });
