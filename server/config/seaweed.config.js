5d1647959d36fa28a0d3e7ddec1ef3b7c3d73dc4
Pehuén <pehuen.rodriguez@nayra.coop>
2020-12-28 21:25:08 -0300

    Some progress on seaweed and added ig login

---
+++ server/config/seaweed.config.js
@@ -0,0 +1,17 @@
+const WeedClient = require("node-seaweedfs");
+
+let seaweedfs;
+
+class SeaweedConfig {
+  static init() {
+    const server = process.env.SEAWEEDFS_MAIN_SERVER || "localhost";
+    const port = process.env.SEAWEEDFS_MAIN_PORT || 9333;
+    seaweedfs = new WeedClient({ server, port });
+  }
+
+  static get() {
+    return seaweedfs;
+  }
+}
+
+module.exports = { SeaweedConfig };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { DBConfig } = require('./config/db.conf');
 const { RoutesConfig } = require('./config/routes.config');
 const { SchedulerConfig } = require('./config/scheduler.config');
 const { CacheConfig } = require('./config/cache.config');
+const { SeaweedConfig } = require('./config/seaweed.config');
 
 const PORT = process.env.API_PORT || 3333;
 const HOST = process.env.API_HOST || 'localhost';
@@ -15,6 +16,7 @@ DBConfig.init();
 RoutesConfig.init(app, express.Router());
 SchedulerConfig.init();
 CacheConfig.init();
+SeaweedConfig.init();
 
 const httpServer = http.createServer(app).listen(PORT, HOST, () => {
   console.log(`Server listening at ${HOST}:${PORT}`);
