3e8bb5779b1c6ca40470f2828b61e399b58a7f05
Pehuén <pehuen.rodriguez@nayra.coop>
2021-01-12 18:08:27 -0300

    Jwt key from config

--- server/api/middleware/auth.js
+++ server/api/middleware/auth.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
+const config = require("config");
+
 const { UserDAO } = require("mv-models");
 
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
+    const jwtKey = config.get("api.jwtKey");
 
-    const data = jwt.verify(token, process.env.JWT_KEY);
+    const data = jwt.verify(token, jwtKey);
     const user = await UserDAO.findOne({ _id: data._id, "tokens.token": token });
     if (!user) {
       throw new Error();
