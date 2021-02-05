3c9392c0e87e5337a594d9b8e6d12730a0ad3519
Pehuén <pehuen.rodriguez@nayra.coop>
2021-02-05 10:31:21 -0300

    #43 new hashtags routes

--- server/api/hashtag/controller.js
+++ server/api/hashtag/controller.js
@@ -1,15 +1,16 @@
 const _ = require("lodash");
 const assert = require("assert");
 
-const { HashtagDAO } = require("mv-models");
+const { ManifestationDAO } = require("mv-models");
 
 class HashtagController {
   async createNew(req, res, next) {
     try {
+      const manifestation = ManifestationDAO.getById(req.params.manifestationId);
       const hashtag = req.body;
       assert(_.isObject(hashtag), "Hashtag is not a valid object.");
 
-      const newHashtag = await HashtagDAO.createNew(hashtag);
+      const newHashtag = await manifestation.newHashtag(hashtag);
       res.status(201).json(newHashtag);
     } catch (error) {
       console.error(error);
@@ -19,7 +20,8 @@ class HashtagController {
 
   async getAll(req, res, next) {
     try {
-      const hashtags = await HashtagDAO.getAll();
+      const manifestation = ManifestationDAO.getById(req.params.manifestationId);
+      const hashtags = await manifestation.getAllHashtags();
       res.status(200).json(hashtags);
     } catch (error) {
       console.error(error);
@@ -27,20 +29,20 @@ class HashtagController {
     }
   }
 
-  async getOne(req, res, next) {
-    try {
-      const hashtag = await HashtagDAO.findById(req.params.hashtagId);
-      if (!hashtag) {
-        return res.status(404).send({
-          message: "hashtag not found with id " + req.params.hashtagId,
-        });
-      }
-      res.status(200).json(hashtag);
-    } catch (error) {
-      console.error(error);
-      next(error);
-    }
-  }
+  // async getOne(req, res, next) {
+  //   try {
+  //     const hashtag = await HashtagDAO.findById(req.params.hashtagId);
+  //     if (!hashtag) {
+  //       return res.status(404).send({
+  //         message: "hashtag not found with id " + req.params.hashtagId,
+  //       });
+  //     }
+  //     res.status(200).json(hashtag);
+  //   } catch (error) {
+  //     console.error(error);
+  //     next(error);
+  //   }
+  // }
 
   async update(req, res, next) {
     try {
@@ -49,20 +51,19 @@ class HashtagController {
           message: "Hashtag content can not be empty",
         });
       }
+      const { manifestationId, hashtagId } = req.params;
+      const manifestation = ManifestationDAO.getById(manifestationId);
 
-      const hashtag = await HashtagDAO.findByIdAndUpdate(
-        req.params.hashtagId,
-        {
-          name: req.body.name,
-        },
-        { new: true },
-      );
+      const hashtag = await manifestation.updateHashtag(hashtagId, {
+        name: req.body.name,
+      });
 
       if (!hashtag) {
         return res.status(404).send({
           message: "hashtag not found with id " + req.params.hashtagId,
         });
       }
+
       res.status(200).json(hashtag);
     } catch (error) {
       console.error(error);
@@ -72,12 +73,17 @@ class HashtagController {
 
   async delete(req, res, next) {
     try {
-      const hashtag = await HashtagDAO.findByIdAndRemove(req.params.hashtagId);
+      const { manifestationId, hashtagId } = req.params;
+
+      const manifestation = ManifestationDAO.getById(manifestationId);
+      const hashtag = await manifestation.deleteHashtag(hashtagId);
+
       if (!hashtag) {
         return res.status(404).send({
           message: "hashtag not found with id " + req.params.hashtagId,
         });
       }
+
       res.status(200).json(hashtag);
     } catch (error) {
       console.error(error);
--- server/api/hashtag/routes.js
+++ server/api/hashtag/routes.js
@@ -1,15 +1,21 @@
 const { HashtagController } = require("./controller");
+const { shapeQuery } = require("../middleware/shape-query");
 const auth = require("../middleware/auth");
 
 class HashtagRoutes {
   static init(router) {
     const hashtagController = new HashtagController();
 
-    router.route("/hashtags").get(hashtagController.getAll);
-    router.route("/hashtags/:hashtagId").get(hashtagController.getOne);
-    router.route("/hashtags").post(auth, hashtagController.createNew);
-    router.route("/hashtags/:hashtagId").put(auth, hashtagController.update);
-    router.route("/hashtags/:hashtagId").delete(auth, hashtagController.delete);
+    router
+      .route("/manifestations/:manifestationId/hashtags")
+      .get([shapeQuery, hashtagController.getAll])
+      .post([auth, hashtagController.createNew]);
+
+    router
+      .route("/manifestations/:manifestationId/hashtags/:hashtagId")
+      // .get(hashtagController.getOne)
+      .put([auth, hashtagController.update])
+      .delete([auth, hashtagController.delete]);
   }
 }
 
