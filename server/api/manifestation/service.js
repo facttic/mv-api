0c4d501c526d48b300202a57dae653e38db47a2a
Pehuén <pehuen.rodriguez@nayra.coop>
2021-02-26 14:22:56 -0300

    facttic/mv-issues#65 Eslint fixes and refactor

--- server/api/manifestation/service.js
+++ server/api/manifestation/service.js
@@ -1,10 +1,10 @@
 const _ = require("lodash");
-const assert = require("assert");
 const { _destroy } = require("bunyan-format");
-const { ManifestationDAO, UserDAO } = require("mv-models");
+const { UserDAO } = require("mv-models");
 
 const s3Service = require("../../common/s3");
 const seaweedHelper = require("../../helpers/seaweed");
+const { NotFoundError, PermissionError } = require("../../helpers/errors");
 
 function parseFieldToArrayElement(object, key, value) {
   const keys = key.split(".");
@@ -61,8 +61,8 @@ async function assingUsers(manifestation) {
   }
 }
 
-async function validateOwnership({ id, name }, { superadmin, manifestation_id }) {
-  if (!superadmin && (!manifestation_id || id.toString() !== manifestation_id.toString())) {
+async function validateOwnership({ id, name }, { superadmin, manifestation_id: manifestationId }) {
+  if (!superadmin && (!manifestationId || id.toString() !== manifestationId.toString())) {
     throw new PermissionError(403, `No tiene permisos de edición para la manifestación ${name}`);
   }
 }
@@ -72,7 +72,7 @@ async function processFiles(manifestation, files) {
 
   // TODO: check for multifiles upload
   // and iterate over the results
-  for (file in files) {
+  for (const file in files) {
     const uploadResults = await s3.client.write(files[file].path);
     const src = seaweedHelper.parseResultsToSrc(uploadResults);
     _.set(manifestation, file, { src });
